c7bfc8fd6045f7e9588f0590f05fec28207db7b8
nicolasnevraumont <nnevraumont@gmail.com>
2019-04-25 16:06:31 +0200

    Open street map display - still to be tuned

--- src/app/components/logs-list-view/logs-list-view.component.ts
+++ src/app/components/logs-list-view/logs-list-view.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/firestore";
 import { Observable } from "rxjs";
 
-import { Location } from "../../shared/models/location"
+import { Coords, Location } from "../../shared/models/location"
+
+declare var ol: any;
 
 @Component({
   selector: 'app-logs-list-view',
   templateUrl: './logs-list-view.component.html',
   styleUrls: ['./logs-list-view.component.scss']
 })
-export class LogsListViewComponent {
+export class LogsListViewComponent implements OnInit {
   private itemsCollection: AngularFirestoreCollection<Location>;
   items: Observable<Location[]>;
+
   constructor(private afs: AngularFirestore) {
     this.itemsCollection = afs.collection<Location>('locations');
     this.items = this.itemsCollection.valueChanges();
@@ -19,5 +22,57 @@ export class LogsListViewComponent {
 
   add(location: Location) {
     this.itemsCollection.add(location);
+  }
+
+  latitude: number = 18.5204;
+  longitude: number = 73.8567;
+
+  map: any;
+
+  ngOnInit() {
+    var mousePositionControl = new ol.control.MousePosition({
+      coordinateFormat: ol.coordinate.createStringXY(4),
+      projection: 'EPSG:4326',
+      // comment the following two lines to have the mouse position
+      // be placed within the map.
+      className: 'custom-mouse-position',
+      target: document.getElementById('mouse-position'),
+      undefinedHTML: '&nbsp;'
+    });
+
+
+    this.map = new ol.Map({
+      target: 'map',
+      controls: ol.control.defaults({
+        attributionOptions: {
+          collapsible: false
+        }
+      }).extend([mousePositionControl]),
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([73.8567, 18.5204]),
+        zoom: 8
+      })
+    });
+
+    this.map.on('click', function (args) {
+      console.log(args.coordinate);
+      var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
+      console.log(lonlat);
+
+      var lon = lonlat[0];
+      var lat = lonlat[1];
+      alert(`lat: ${lat} long: ${lon}`);
+    });
+  }
+
+  showOnMap(coords: Coords) {
+    var view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([coords.longitude, coords.latitude]));
+    view.setZoom(12);
   }
 }
--- src/app/shared/models/address.ts
+++ src/app/shared/models/address.ts
@@ -1,11 +1,17 @@
-export interface Address {
+export class Address {
   road?: string;
   village?: string;
+  town?: string
   state_district?: string;
   state?: string;
   postcode?: string;
   country?: string;
   country_code?: string;
+
+  public get location_name(): string {
+    console.log(this.village);
+    return (this.village !== null) ? this.village : this.town;
+  }
 }
 
 export interface OpenStreetMapResponse {
